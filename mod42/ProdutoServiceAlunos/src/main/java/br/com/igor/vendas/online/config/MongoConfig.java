c0b1c89238e1537e0085ff07fe5e17d34db33078
IgorMinozzo <igoorminozzo000@gmail.com>
2024-06-28 13:45:52 -0300

    Tarefa mod42

---
+++ mod42/ProdutoServiceAlunos/src/main/java/br/com/igor/vendas/online/config/MongoConfig.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package br.com.igor.vendas.online.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+
+/**
+ * @author rodrigo.pires
+ *
+ */
+@Configuration
+@EnableMongoRepositories(basePackages = "br.com.igor.vendas.online.repository")
+public class MongoConfig {
+
+}
